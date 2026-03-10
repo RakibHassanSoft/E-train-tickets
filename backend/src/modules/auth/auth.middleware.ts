b07cf3d75f2e615cff7462929210a73c1d1cc183
RakibHassanSoft <rakibreings32@gmail.com>
2026-03-10 20:37:25 +0600

    (create):authorizeRoles middleware is added

--- backend/src/modules/auth/auth.middleware.ts
+++ backend/src/modules/auth/auth.middleware.ts
@@ -1,4 +1,13 @@
 import { verifyAccessToken } from "../../utils/jwt";
+import { Request, Response, NextFunction } from "express";
+import { sendResponse } from "../../utils/sendResponse";
+
+interface AuthRequest extends Request {
+  user?: {
+    userId: string;
+    role: string;
+  };
+}
 
 export const authMiddleware = (req: any, res: any, next: any) => {
 
@@ -21,4 +30,50 @@ export const authMiddleware = (req: any, res: any, next: any) => {
   } catch {
     res.status(401).json({ message: "Invalid token" });
   }
+};
+
+export const authorizeRoles = (allowedRoles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    const auth = req.headers.authorization;
+    if (!auth) return res.status(401).json({ message: "Unauthorized" });
+
+    const token = auth.split(" ")[1];
+
+    try {
+      const decoded = verifyAccessToken(token);
+
+      // Ensure decoded has userId and role
+      if (typeof decoded === "string") {
+        return sendResponse({
+          res,
+          statusCode: 401,
+          success: false,
+          message: "Invalid token payload",
+        });
+      }
+
+      const { userId, role } = decoded as { userId: string; role: string };
+
+      req.user = { userId, role };
+
+      // Check role
+      if (!allowedRoles.includes(role)) {
+        return sendResponse({
+          res,
+          statusCode: 403,
+          success: false,
+          message: "Forbidden: Insufficient role",
+        });
+      }
+
+      next();
+    } catch (err) {
+      return sendResponse({
+        res,
+        statusCode: 401, 
+        success: false,
+        message: "Invalid token",
+      });
+    }
+  };
 };
--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import * as controller from "./user.controller";
-import { authMiddleware } from "../auth/auth.middleware";
+import { authMiddleware, authorizeRoles } from "../auth/auth.middleware";
 import catchAsync from "../../utils/catchAsync";
 
 
 const userRoutes = Router();
 
 
-userRoutes.get("/me", authMiddleware, catchAsync(controller.profile));
+userRoutes.get("/me", authMiddleware,authorizeRoles(["PASSENGER"]) ,catchAsync(controller.profile));
 
 userRoutes.put("/me", authMiddleware, catchAsync(controller.updateProfile));
 
